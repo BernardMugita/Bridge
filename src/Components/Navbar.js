ea208f12dee6392e57433caea3d2dd13099c5f24
BernardMugita <mugita68@gmail.com>
2021-08-23 14:43:35 +0300

    This is the first commit for the project containing the basic react components and resources in javascript and css

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../Resources/Logo.png'
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="leftside"></div>
+            <div className="rightside"></div>
+                <img src="{Logo}"/>
+        </div>
+    )
+}
+
+export default Navbar
