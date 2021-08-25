291b08a25919150ce52a0bb133732e849074d195
BernardMugita <mugita68@gmail.com>
2021-08-25 09:36:55 +0300

    This is the code for the landing page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import Logo from '../Resources/Logo.png'
+import React, { useState } from 'react';
+import Logo from '../Resources/Logo.png';
+import { Link } from "react-router-dom";
+import ReorderIcon from '@material-ui/icons/Reorder';
+import "../Styles/Navbar.css";
+
 function Navbar() {
+
+    const [openLinks, setOpenLinks] = useState(false)
+
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks)
+    }
     return (
         <div className="navbar">
-            <div className="leftside"></div>
-            <div className="rightside"></div>
-                <img src="{Logo}"/>
+            <div className="leftSide" id={openLinks ? "open" : "close"}>
+                <img src={Logo} alt="Bridge"/>
+                <div className="hiddenLinks">
+                    <Link to ="/"> Home </Link>
+                    <Link to ="/Artists"> Artists </Link>
+                    <Link to ="/Login"> Login </Link>
+                    <Link to ="/Contact-us"> Contact </Link>
+                </div>
+            </div>
+            <div className="rightSide">
+                <Link to ="/"> Home </Link>
+                <Link to ="/Artists"> Artists </Link>
+                <Link to ="/Login"> Login </Link>
+                <Link to ="/Contact-us"> Contact </Link>
+                <button onClick={toggleNavbar}>
+                <ReorderIcon/>
+                </button>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
